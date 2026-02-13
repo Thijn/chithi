060ff1d681dc04b878888f6de3e1091fc105596b
baseplate-admin <61817579+baseplate-admin@users.noreply.github.com>
2026-02-13 10:50:37 +0600

    feat: use public urls

--- apps/landing-page/src/app/scalar/page.tsx
+++ apps/landing-page/src/app/scalar/page.tsx
@@ -1,14 +1,14 @@
-'use client'
-import { ApiReferenceReact } from '@scalar/api-reference-react'
-import '@scalar/api-reference-react/style.css'
-import openapi from "./openapi.json";
+'use client';
+import { ApiReferenceReact } from '@scalar/api-reference-react';
+import { OPENAPI_URL } from '@/consts/urls';
+import '@scalar/api-reference-react/style.css';
 
 export default function References() {
-  return (
-    <ApiReferenceReact
-      configuration={{
-        content: openapi
-      }}
-    />
-  )
+    return (
+        <ApiReferenceReact
+            configuration={{
+                url: OPENAPI_URL,
+            }}
+        />
+    );
 }
--- apps/landing-page/src/consts/urls.ts
+++ apps/landing-page/src/consts/urls.ts
@@ -1,1 +1,2 @@
-export const PUBLIC_INSTANCE_URL = 'https://public.chithi.dev/status/public'
+export const PUBLIC_INSTANCE_URL = 'https://public.chithi.dev/status/public';
+export const OPENAPI_URL = `https://chithi.dev/api/openapi.json`;

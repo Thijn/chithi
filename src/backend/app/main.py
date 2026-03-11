a142b7f9a006aa357599be84530546463c5d334e
baseplate-admin <61817579+baseplate-admin@users.noreply.github.com>
2026-03-11 19:31:17 +0600

    refractor: move files around

--- src/backend/app/main.py
+++ src/backend/app/main.py
@@ -48,54 +48,54 @@ app.add_middleware(
     allow_credentials=True,
 )
 
-from app.routes.admin.config import router as admin_config_router
+from app.routes.http.admin.config import router as admin_config_router
 
 app.include_router(admin_config_router, prefix="/admin")
 
-from app.routes.admin.user import router as admin_user_router
+from app.routes.http.admin.user import router as admin_user_router
 
 app.include_router(admin_user_router, prefix="/admin")
 
-from app.routes.admin.files import router as admin_file_router
+from app.routes.http.admin.files import router as admin_file_router
 
 app.include_router(admin_file_router, prefix="/admin")
 
-from app.routes.config import router as config_router
+from app.routes.http.config import router as config_router
 
 app.include_router(config_router)
 
-from app.routes.login import router as login_router
+from app.routes.http.login import router as login_router
 
 app.include_router(login_router)
 
-from app.routes.user import router as user_router
+from app.routes.http.user import router as user_router
 
 app.include_router(user_router)
 
-from app.routes.upload import router as upload_router
+from app.routes.http.upload import router as upload_router
 
 app.include_router(upload_router)
 
-from app.routes.download import router as download_router
+from app.routes.http.download import router as download_router
 
 app.include_router(download_router)
 
-from app.routes.information import router as information_router
+from app.routes.http.information import router as information_router
 
 app.include_router(information_router)
 
-from app.routes.onboarding import router as onboarding_router
+from app.routes.http.onboarding import router as onboarding_router
 
 app.include_router(onboarding_router)
 
-from app.routes.speedtest import router as speedtest_router
+from app.routes.http.speedtest import router as speedtest_router
 
 app.include_router(speedtest_router)
 
-from app.routes.token import router as token_router
+from app.routes.http.token import router as token_router
 
 app.include_router(token_router)
 
-from app.routes.ws import router as ws_router
+from app.routes.ws.state import router as ws_router
 
 app.include_router(ws_router)

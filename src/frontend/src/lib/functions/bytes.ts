3f34c249b32ce496fd0ad5ef13efa2a9c7c98ba4
Thijn <thijn38@gmail.com>
2026-04-18 23:49:29 +0200

    Refactor formatFileSize to use formatBytes

--- src/frontend/src/lib/functions/bytes.ts
+++ src/frontend/src/lib/functions/bytes.ts
@@ -23,8 +23,6 @@ export function bytesToNumber(value: number, unit: ByteUnit): number {
 }
 export const formatFileSize = (bytes: number): string => {
 	if (bytes === 0) return '0 Bytes';
-	const k = 1024;
-	const sizes = ['Bytes', 'KB', 'MB', 'GB'];
-	const i = Math.floor(Math.log(bytes) / Math.log(k));
-	return parseFloat((bytes / Math.pow(k, i)).toFixed(2)) + ' ' + sizes[i];
+	const format = formatBytes(bytes);
+	return format.val + ' ' + format.unit;
 };
